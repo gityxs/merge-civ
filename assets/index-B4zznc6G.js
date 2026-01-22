129202e06799fae73b790bcb99899881e17b642b
zhaolinxu <zhaolinxu@sina.cn>
2026-01-22 10:12:23 +0800

    gx 1.2.10

---
+++ assets/index-B4zznc6G.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-C-b9WZKg.js","assets/index-Cug0PJkf.js","assets/index-VDsp-TWB.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-Cug0PJkf.js";const _=p("App",{web:()=>r(()=>import("./web-C-b9WZKg.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
