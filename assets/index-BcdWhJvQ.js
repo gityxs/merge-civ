ef8f36f5e8195b2c776bb2cccabf6d718c01105d
zhaolinxu <zhaolinxu@sina.cn>
2026-03-03 14:41:04 +0800

    gx 1.5.4

---
+++ assets/index-BcdWhJvQ.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Bo_3p0jx.js","assets/index-C1T1BN4n.js","assets/index-BATH9sES.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-C1T1BN4n.js";const _=p("App",{web:()=>r(()=>import("./web-Bo_3p0jx.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
